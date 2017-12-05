3e8ccfec58f66ca6b2968af1424534780eb688d8
Adem Yenice <yenicead@itu.edu.tr>
2017-12-05 23:54:16 +0300

    I added javascript code to index page.

--- server.py
+++ server.py
@@ -446,6 +446,6 @@ if __name__ == '__main__':
         app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
     else:
         app.config['dsn'] = """user='vagrant' password='vagrant'
-                               host='localhost' port=5433 dbname='itucsdb'"""
+                               host='localhost' port=5432 dbname='itucsdb'"""
 
     app.run(host='0.0.0.0', port=port, debug=debug)
